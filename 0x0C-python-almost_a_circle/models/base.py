5157338761cd0e324c25e7383aa2f7bca3ce997d
Kaych3e <nkaduba@gmail.com>
2023-02-13 15:02:50 +0000

    Circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Defines a base model class."""
- import json
- import csv
- import turtle
+import json
+import csv
+import turtle
 
 
- class Base:
+class Base:
     """Represent the base model.
 
     Represents the "base" for all other classes in project 0x0C*.
