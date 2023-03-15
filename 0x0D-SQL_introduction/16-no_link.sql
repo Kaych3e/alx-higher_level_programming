e3df8da33a8a0aa3804c07e3e96f61984d2c225f
Kaych3e <nkaduba@gmail.com>
2023-03-15 00:49:19 +0000

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- Lists all records of the table second_table with name value
 -- Records are listed by descending score
-SELECT score, name FROM second_table
-WHERE name != NULL
+SELECT score, name
+FROM second_table
+WHERE name != ""
 ORDER BY score DEC;
