b5c9f75686e1e7dbbe5a0647e48ce1487cd820d4
Kaych3e <nkaduba@gmail.com>
2023-03-15 13:00:47 +0000

    SQL

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Creates a table with its description. id, name cannot be NULL
+-- Database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
