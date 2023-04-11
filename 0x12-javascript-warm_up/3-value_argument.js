2d6716b0c6a36e65de451f06381577249c920948
Kaych3e <nkaduba@gmail.com>
2023-04-11 21:19:00 +0000

    JS

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that prints the first argument passed to it with console.log
+// No use of 'var' and 'length'
+
+const args = process.argv.slice(2);
+const Arg1 = args[0];
+//passes the argument to the script and checks if true or false
+
+if (!Arg1) {
+    console.log(Arg1);
+} else {
+    console.log('No Argument');
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that prints two arguments passed to it, in the following format: is
+
+function printls(arg1, arg2){
+    console.log(`${arg1} is ${arg2}`);
+}
