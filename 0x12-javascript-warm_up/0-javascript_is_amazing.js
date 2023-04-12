6b6ddb0eb4ec3acc64fea3467b16623bb9bb048b
Kaych3e <nkaduba@gmail.com>
2023-04-12 14:20:56 +0000

    JS

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+
 // Write a script that prints JavaScript is amazing
 // using console.log to print output. var not allowed.
+
 const myVar = 'JavaScript is amazing';
   console.log(myVar);
