4a2ba3c05814f537b3b021442f0bd69f2f1efc48
root <nkaduba@gmail.com>
2023-04-13 13:11:28 -0700

    JS Objects

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,13 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
-    }
-
+    if ((w > 0) && (h > 0)) {
     // setting instance attributes
-    this.width = w;
-    this.width = h;
+      this.width = w;
+      this.width = h;
+    }
   }
 
   // creates instance method print() to print the rectangle with X
@@ -16,9 +14,9 @@ class Rectangle {
     let row = '';
     for (let i = 0; i < this.width; i++) {
       row += 'X';
-    }
-    for (let j = 0; j < this.height; j++) {
-      console.log(row);
+      for (let j = 0; j < this.height; j++) {
+        console.log(row);
+      }
     }
   }
 
