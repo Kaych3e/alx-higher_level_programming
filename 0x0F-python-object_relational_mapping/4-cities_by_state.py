625fb65a3100384be2cb97c0dafa7655a8b5bef6
Kaych3e <nkaduba@gmail.com>
2023-04-16 21:55:26 +0000

    ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = db.cursor()
-    query = "".join([
+    query = " ".join([
     "SELECT cities.id, cities.name, states.name FROM cities",
     "INNER JOIN states ON states.id = cities.state_id",
     "ORDER BY cities.id"])
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,14 +8,16 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    query = "SELECT * FROM cities", "WHERE state_id =
-    (SELECT id FROM states WHERE name LIKE %s)", "ORDER BY cities.id",
-    state_name = (sys.argv[4],)
-    cur.execute(query, state_name)
-
+    query = " ".join([
+        "SELECT cities.name FROM cities",
+       	"INNER JOIN states ON states.id = cities.state_id",
+	"WHERE states.name LIKE BINARY '{}'",
+        "ORDER BY cities.id",
+    ]).format(sys.argv[4])
+    cur.execute(query)
     data = cur.fetchall()
-    for row in data:
-        print(row)
+    strdata = ', '.join([i[0] for i in data])
+        print(strdata)
 
     cur.close()
     db.close()
