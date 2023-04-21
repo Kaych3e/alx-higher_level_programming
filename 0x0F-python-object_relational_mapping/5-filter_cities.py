be7312aad87619dc56f29b4328fc205b11e5adc2
root <nkaduba@gmail.com>
2023-04-21 12:38:03 -0700

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa"""
+"""
+Script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+Execution: <mysql username> \
+           <mysql password> \
+           <database name> \
+           <state name searched>
+"""
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     query = " ".join([
         "SELECT cities.name FROM cities",
