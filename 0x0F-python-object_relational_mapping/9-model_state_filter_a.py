fecb1f6858b74354c81ba0b3b407c632eead658e
root <nkaduba@gmail.com>
2023-04-21 13:35:22 -0700

    ORM

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,6 +2,9 @@
 """
 Lists all State objects that contain the letter a
 from the database hbtn_0e_6_usa
+Script argument: <mysql username> \
+                 <mysql password> \
+                 <database name> \
 """
 import sys
 from sqlalchemy import create_engine
@@ -9,23 +12,22 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     """
     Creates the engine that connects to the database,
     a session to communicate with the database, and
     Queries the database for all states that contain the letter 'a'
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db_name),
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-st = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
-
-for state in st:
-    print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
