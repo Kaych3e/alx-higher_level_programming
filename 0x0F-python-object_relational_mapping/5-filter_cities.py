e86932b688a18908553f369e97cd6b0e06eac28c
root <nkaduba@gmail.com>
2023-04-21 12:47:44 -0700

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,10 +2,6 @@
 """
 Script that takes in the name of a state as an argument
 and lists all cities of that state, using the database hbtn_0e_4_usa
-Execution: <mysql username> \
-           <mysql password> \
-           <database name> \
-           <state name searched>
 """
 import MySQLdb
 import sys
@@ -13,16 +9,8 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = " ".join([
-        "SELECT cities.name FROM cities",
-        "INNER JOIN states ON states.id = cities.state_id",
-        "WHERE states.name LIKE BINARY '{}'",
-        "ORDER BY cities.id",
-    ]).format(sys.argv[4])
-    cur.execute(query)
-    data = cur.fetchall()
-    strdata = ', '.join([i[0] for i in data])
-print(strdata)
-
-cur.close()
-db.close()
+    cur.execute("SELECT * FROM `cities` as `city` \
+                 INNER JOIN `states` as `state` \
+                    ON `city`.`state_id = `state`.`id` \
+                 ORDER BY `city`.`id`")
+    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
