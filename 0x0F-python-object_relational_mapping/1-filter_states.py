9856a8ac4613832af75533ea7d49656c67eebebf
root <nkaduba@gmail.com>
2023-04-21 12:22:35 -0700

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,20 +2,12 @@
 """script that lists all states with a name starting with N
     from the database hbtn_0e_0_usa"""
 import sys
-import MySQLdb from MySQLdb
+import MySQLdb
 
 if __name__ == '__main__':
 
     """ Connect to MySQL server and create cursor object"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
-    data = cur.fetchall()
-    for row in data:
-        if row[1][0] == "N"
-print(row)
-
-cur.close()
-db.close()
+    [print(state) for state in cur.fetchall() if state[1][0] == 'N']
