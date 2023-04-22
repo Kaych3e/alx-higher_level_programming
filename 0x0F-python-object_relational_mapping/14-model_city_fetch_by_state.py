81070d8653642d2222122314743ea289623c6f4f
root <nkaduba@gmail.com>
2023-04-22 04:31:20 -0700

    ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(mysql_username, mysql_password, db_name),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
@@ -29,5 +29,3 @@ if __name__ == "__main__":
     cities = session.query(City).order_by(City.id).all()
     for city in cities:
         print('{}: ({}) {}'.format(city.state.name, city.id, city.name))
-
-    session.close()
