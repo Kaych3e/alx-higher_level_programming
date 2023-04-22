2ad5829265bd0293c8491198d4a2084b3f80d655
root <nkaduba@gmail.com>
2023-04-22 04:29:04 -0700

    ORM

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -13,6 +13,6 @@ Base = declarative_base()
 class City(Base):
     """Class definition of a City"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
