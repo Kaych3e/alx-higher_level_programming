f77a13591768fa7a17802c0b5b9e621454c6c839
root <nkaduba@gmail.com>
2023-04-27 14:18:11 -0700

    PN1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-# script that takes in a URL, sends a request to the URL displays the value
-# of the X-Request-Id variable found in the header of the response.
+# sends request to the URL displays the value of X-Request-Id variable
 
-import sys
 import urllib.request
+import sys
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io") as response:
         headers = response.info()
         print(headers['X-Request-Id'])
