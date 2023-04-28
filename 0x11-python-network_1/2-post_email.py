10059712f5b0446c44d278ad507b955d2a5ea1cb
root <nkaduba@gmail.com>
2023-04-28 06:54:23 -0700

    PN1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,10 @@
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)"""
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
 
-
+if __name__ == "__main__":
 url = sys.argv[1]
 email = sys.argv[2]
 
