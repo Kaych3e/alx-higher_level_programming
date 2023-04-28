99c786e43aa49a76e570c710c65d321f7de91cd5
root <nkaduba@gmail.com>
2023-04-28 08:35:38 -0700

    PN1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,13 +4,10 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
+
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
 
-    if response.status_code == 200:
-        print('-' * 30)
-        print('Body response:')
-        print('\t- type:', type(response.text))
-        print('\t- content:', response.text)
-    else:
-        print(f"Error code: {response.status_code}")
+    print('Body response:')
+    print(f'\t- type: {type(response.text)}')
+    print(f'\t- content: {response.text}')
