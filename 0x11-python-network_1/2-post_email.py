8c00534a235da476cbf45e95cd8cfd80f4270c2f
root <nkaduba@gmail.com>
2023-04-28 07:01:00 -0700

    PN1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,14 +7,14 @@ import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
-url = sys.argv[1]
-email = sys.argv[2]
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
 
+    """encode email as a parameter"""
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
 
-"""encode email as a parameter"""
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(req) as response:
-    page = response.read().decode('utf-8')
-    print(page)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode('utf-8')
+        print(the_page)
