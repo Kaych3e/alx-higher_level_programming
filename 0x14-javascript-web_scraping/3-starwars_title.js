06228003660ae0e8bf01d6ea8f502be7449ca6ce
root <nkaduba@gmail.com>
2023-05-23 15:19:28 -0700

    T3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
-#!#!
+#!/usr/bin/node
+// Script that prints the title of a Star Wars movie
+
+// import request and request URL
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (error, response, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(data).title);
+  }
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+//script that prints the number of movies
+//where the character Wedge Antilles is present
+
