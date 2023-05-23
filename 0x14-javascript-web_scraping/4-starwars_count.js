08769b4b7e15d93428d531e79942f1f24c44b752
root <nkaduba@gmail.com>
2023-05-23 15:48:20 -0700

    T4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
-//script that prints the number of movies
-//where the character Wedge Antilles is present
+// Script that prints the number of movies
+// where the character Wedge Antilles is present.
 
+// import request and request URL
+const request = require('request');
+const url = process.argv[2];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(url, (error, response, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = (JSON.parse(data).results);
+    const match = films.filter((film) => film.characters.includes(character));
+    console.log(`${match.length}`);
+  }
+});
