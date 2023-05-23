51c73154662514dbc99a4db7ab5fda88db14af33
root <nkaduba@gmail.com>
2023-05-23 14:04:30 -0700

    T1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, { encoding: 'utf-8' }, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
