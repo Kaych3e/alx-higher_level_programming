dede3e58aa025985d90109fce6d760c4914c39d9
root <nkaduba@gmail.com>
2023-05-23 16:21:19 -0700

    T5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage and stores it in a file.
+// script that gets the contents of a webpage and stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, 'utf-8', (error, response, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFile(filePath, data, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
