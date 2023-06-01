8973f25869447c14fe4aecc6dcacea7e74a9b973
root <nkaduba@gmail.com>
2023-06-01 12:50:28 -0700

    jQuery

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,3 @@
-$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-  $(data.results).each(function () {
-    $('UL#list_movies').append('<li>' + this.title + '<li>');
-  });
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
+  $('DIV#hello').text(data.hello);
 });
